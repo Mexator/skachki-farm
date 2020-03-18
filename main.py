37d62af08f10ed4b2867a01dfe828fe8bfa92492
Anton Brisilin <brisilant@yandex.ru>
2020-03-18 18:18:18 +0300

    fixed error with freezing when unable send message

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ api_hash = 'XXX'
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
 async def main():
+    entity = await client.get_entity('https://t.me/Horse_run_bot')
+    waiting_multiplier=1
     while True:
-        entity = await client.get_entity('https://t.me/Horse_run_bot')
-        await client.send_message(entity=entity,message='/bonus')
-        print('Message was sent')
-        time.sleep(60*60/2)
+        try:
+            await asyncio.wait_for(client.send_message(entity=entity,message='/bonus'),timeout=3.0)
+            print('Message was sent')
+            time.sleep(60*60)
+            waiting_multiplier=1
+        except asyncio.TimeoutError:
+            print('Error sending message, retry in',10*waiting_multiplier,'seconds')
+            await asyncio.sleep(10*waiting_multiplier)
+            waiting_multiplier*=2
 asyncio.get_event_loop().run_until_complete(main())
